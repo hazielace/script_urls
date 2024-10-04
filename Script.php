76c6af2408bce2e2ae6e5f08028cd256c4341033
hazielace <107333058+hazielace@users.noreply.github.com>
2024-10-04 04:46:36 -0500

    Update Script.php

--- Script.php
+++ Script.php
@@ -1,20 +1,22 @@
 <?php
 
 function procesarUrls($archivo) {
-    $urls_validate = [];
-    $patron = "/shop/";
+    $urls_validate = []; // Array para eliminar duplicados
+    $patron = "/shop/"; // Expresión regular para dominios con "shop"
+    // Abrir el archivo
     if (($handle = fopen($archivo, "r")) !== false) {
         while (($linea = fgets($handle)) !== false) {
-            $url = trim($linea);
+            $url = trim($linea); // Limpiar los espacios o saltos de línea
+            // Valida si la URL contiene "shop" en el dominio y si termina en ".html"
             if (preg_match($patron, $url) && substr($url, -5) === ".html") {
-                $urls_validate[$url] = true;
+                $urls_validate[$url] = true; // Se almacena las URLS duplicadas para eliminarlas
             }
         }
         fclose($handle);
     }
-
+    // Muestra la cantidad de URLS válidas con "count"
     echo "Total de URLS válidas son: " . count($urls_validate) . PHP_EOL;
-
+    // Lista todas las URLS válidas
     foreach ($urls_validate as $url => $value) {
         echo $url . PHP_EOL;
     }
